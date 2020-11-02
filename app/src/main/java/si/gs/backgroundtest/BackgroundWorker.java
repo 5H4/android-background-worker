8b8fa8903215e3f9abcd75973c04d3b565f9a87a
5H4 <ramo.toric@gmail.com>
2020-11-02 18:51:44 +0100

    Update BackgroundWorker.java
    so, if u need to transfer you data from app to background use:
    sharedpreferences, or some local logic...

--- app/src/main/java/si/gs/backgroundtest/BackgroundWorker.java
+++ app/src/main/java/si/gs/backgroundtest/BackgroundWorker.java
@@ -8,20 +8,35 @@ import androidx.annotation.NonNull;
 import androidx.work.Worker;
 import androidx.work.WorkerParameters;
 
-public class BackgroundWorker extends Worker {
+public class BackgroundService extends IntentService {
     private static final String TAG = "BackgroundWorker";
-
+    
+    /*
     public BackgroundWorker(
             @NonNull Context context,
             @NonNull WorkerParameters params) {
         super(context, params);
-    }
+    }*/
 
+    /*
     @Override
     public Result doWork() {
         String text = "Application executing background work";
         Log.d(TAG, text);
         return Result.success();
+    }*/
+    
+    protected void onHandleIntent(Intent service) {
+        Log.d("TAG", service.getDataString());
     }
+    
+    /*
+    Add to manifest:
+    Start your serivce in background...
+    <application>
+        <service
+                android:name=".BackgroundService"
+                android:exported="false"/>
+    </application>
+    */
 }
-
